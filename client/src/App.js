d6af34aaebba0f748466cb9ab5f93d3acacf5568
dharani <dchan@DC-PC.localdomain>
2022-03-15 22:18:05 -0400

    aws file update000

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React tezetxgg
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
